a15d994886ca2d6fd0cfce5279ca7db7295b6f1f
scott <scott@scott.(none)>
2011-03-26 16:45:58 -0400

    moved the sketch to a seperate file

--- canvasplayer.js
+++ canvasplayer.js
@@ -89,46 +89,7 @@ function load_gml(data)
 
     var app_name = data.gml.tag.header  &&  data.gml.tag.header.client  &&  data.gml.tag.header.client.name;
 
-		document.getElementById('sketch'+i).innerHTML = ajax("gmlplayer.js");/*" \
-		void setup() { \
-		  size(600, 500); \
-		  frameRate(30); \
-      background(0); \
-      noLoop(); \
-		  if('"+app_name+"' == 'Graffiti Analysis 2.0: DustTag' || '"+app_name+"' == 'DustTag: Graffiti Analysis 2.0' || '"+app_name+"' == 'Fat Tag - Katsu Edition'){ \
-  			rotation = 80; \
-  			translation = [0, 500]; \
-		  } else { \
-			  rotation = 0; \
-			  translation = [0, 0]; \
-			} \
-      pts" + i + " = pts; \
-      pts_opts" + i + " = pts_opts; \
-      strokeCount"+i+" = 0; \
-		}; \
-		void draw() { \
-		  i = frameCount % pts"+i+".length; \
-		  prev = pts"+i+"[i-1]; \
-		  pt = pts"+i+"[i]; \
-      if(i == 0){ background(0); } \
-		  if(pt == undefined || pt == []){ \
-  			strokeCount"+i+"++; \
-  			return; \
-		  } \
-		  if(prev == undefined || prev == []){ \
-  			prev = pt; \
-		  } \
-		  dimx = (prev.x -pt.x)*width; \
-		  dimy = (prev.y -pt.y)*height; \
-  		hyp = 1/(sqrt(pow(dimx,2),pow(dimy,2)) + 20); \
-		  translate(translation[0], translation[1]); \
-		  rotate(rotation); \
-		  num = (pow(hyp,0.5)*90); \
-  		strokeWeight(num); \
-      var colors = ['255','255','255']; \
-		  stroke(colors[0],colors[1],colors[2]); \
-		  line(prev.x*width, prev.y*height, pt.x*width, pt.y*height); \
-		}";*/
+		document.getElementById('sketch'+i).innerHTML = ajax("gmlplayer.js");
 		
 		//var canvas 	= document.getElementById('canvas' + i); // single canvas for every tag
 		var canvas 	= document.getElementById( 'canvas' + data.id ); // one canvas for all tags
@@ -136,7 +97,7 @@ function load_gml(data)
 
 		// draw sketch on canvas
 		new Processing(canvas, sketch);
-    //Processing.getInstanceById( 'canvas' + data.id ).manSetup( app_name, i );
+    Processing.getInstanceById( 'canvas' + data.id ).manSetup( app_name, i );
         
 	}
 	else
--- gmlplayer.js
+++ gmlplayer.js
@@ -1,74 +1,46 @@
 // Global variables  
-float radius = 50.0;  
-int X, Y;  
-int nX, nY;  
-int delay = 16;  
+String app_name = "";  
+int index = 0;
+var doneSetup = false;
+var data_object = {};
   
 // Setup the Processing Canvas  
 void setup(){  
-  size( 200, 200 );  
-  strokeWeight( 10 );  
-  frameRate( 15 );  
-  X = width / 2;  
-  Y = height / 2;  
-  nX = X;  
-  nY = Y;    
-}  
-  
-// Main draw loop  
-void draw(){  
-    
-  radius = radius + sin( frameCount / 4 );  
-    
-  // Track circle to new destination  
-  X+=(nX-X)/delay;  
-  Y+=(nY-Y)/delay;  
-    
-  // Fill canvas grey  
-  background( 100 );  
-    
-  // Set fill-color to blue  
-  fill( 0, 121, 184 );  
-    
-  // Set stroke-color white  
-  stroke(255);   
-    
-  // Draw circle  
-  ellipse( X, Y, radius, radius );                    
-}  
-  
-  
-// Set circle's next destination  
-void mouseMoved(){  
-  nX = mouseX;  
-  nY = mouseY;    
-} 
-
-/*    void setup() {
 		  size(600, 500); 
 		  frameRate(30); 
       background(0); 
       noLoop(); 
-    };
-		void manSetup( app_name, index ) { 
-		  if(app_name === 'Graffiti Analysis 2.0: DustTag' || app_name === 'DustTag: Graffiti Analysis 2.0' || app_name === 'Fat Tag - Katsu Edition'){ 
+}  
+
+void manSetup( app_nameNew, indexNew ){
+      //console.log( app_nameNew );
+      app_name = app_nameNew;
+      index = indexNew;
+		  if(app_name == 'Graffiti Analysis 2.0: DustTag' || app_name == 'DustTag: Graffiti Analysis 2.0' || app_name == 'Fat Tag - Katsu Edition'){ 
   			rotation = 80; 
   			translation = [0, 500]; 
 		  } else { 
 			  rotation = 0; 
 			  translation = [0, 0]; 
 			} 
-      pts + index + = pts; 
-      pts_opts + index +  = pts_opts; 
-      strokeCount+index+ = 0; 
-		}; 
-		void draw() { 
-		  i = frameCount % pts+index+.length; 
-		  prev = pts+index+[i-1]; 
-		  pt = pts+index+[i]; 
+      //eval( "pts" + index + " = pts"); 
+      //pts_opts + index +  = pts_opts; 
+      //strokeCount+index+ = 0;
+      data_object["pts" + index] = pts;
+      data_object["pts_opts" + index ] = pts_opts;
+      data_object["strokeCount" + index ] = 0;
+      doneSetup = true;
+}
+  
+// Main draw loop  
+void draw(){
+  if (doneSetup) {
+		  i = frameCount % data_object["pts"+index].length; 
+		  prev = data_object["pts"+index][i-1]; 
+		  pt = data_object["pts"+index][i]; 
       if(i == 0){ background(0); } 
 		  if(pt == undefined || pt == []){ 
-  			strokeCount+i)++; 
+  			data_object["strokeCount" + index ]++; 
   			return; 
 		  } 
 		  if(prev == undefined || prev == []){ 
@@ -83,5 +55,8 @@ void mouseMoved(){
   		strokeWeight(num); 
       var colors = ['255','255','255']; 
 		  stroke(colors[0],colors[1],colors[2]); 
-		  line(prev.x*width, prev.y*height, pt.x*width, pt.y*height); 
-		};*/
+		  line(prev.x*width, prev.y*height, pt.x*width, pt.y*height);
+  }
+}  
+ 
+
